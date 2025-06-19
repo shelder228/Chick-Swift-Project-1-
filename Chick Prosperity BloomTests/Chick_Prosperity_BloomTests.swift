c85f1c909184cc619748a415c7f61c1e41841515
leshabl <shelder228@gmail.com>
2025-06-19 22:32:28 +0300

    221

---
+++ Chick Prosperity BloomTests/Chick_Prosperity_BloomTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chick_Prosperity_BloomTests.swift
+//  Chick Prosperity BloomTests
+//
+//  Created by Олексій on 19.06.2025.
+//
+
+import Testing
+@testable import Chick_Prosperity_Bloom
+
+struct Chick_Prosperity_BloomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
